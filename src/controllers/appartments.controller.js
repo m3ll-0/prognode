8d25d5cce444d95d84f51489afd824bbe1d4c79b
totatevenaar <jordyvanekelen1@gmail.com>
2019-05-10 17:24:00 +0200

    Created core functionality. Created SQL connection. Created AppartmentRoutes.

---
+++ src/controllers/appartments.controller.js
@@ -0,0 +1,231 @@
+// Import database
+// Route controllers
+
+const database = require('../datalayer/mssql.dao');
+
+module.exports = {
+
+    getAllAppartments: (req, res, next) => {
+        
+        console.log("getAllAppartments called");
+
+        const query = "SELECT * FROM Apartment JOIN Reservation ON Apartment.ApartmentId = Reservation.ApartmentId JOIN DBUser ON Reservation.UserId = DBUser.UserId;";
+
+        database.executeQuery(query, (err, rows) => {
+            // handle result
+            if(err){
+                const errorObject = {
+                    message: 'Er is iets misgegaan',
+                    code: 500
+                }
+                next(errorObject);
+            }
+            if(rows){
+                res.status(200).json({result: rows});
+            }
+        })
+    },
+
+    createAppartment: (req, res, next) => {
+        console.log("createAppartment called");
+
+        const appartment = req.body;
+        const query = `INSERT INTO Apartment VALUES('${appartment.Description}', '${appartment.StreetAddress}', '${appartment.PostalCode}', '${appartment.City}', ${appartment.UserId} )`;
+
+        database.executeQuery(query, (err, rows) => {
+
+            if(err){
+                const errorObject = {
+                    message: 'Er is iets misgegaan',
+                    code: 500
+                }
+                next(errorObject);
+            }else{
+                res.status(200).json({result: 200});
+            }
+        })
+    },
+
+    getAppartment: (req, res ,next) => {
+        console.log("getAppartment called");
+
+        const id = req.params.id;
+        const query = `SELECT * FROM Apartment WHERE Apartment.ApartmentId=${id}`;
+
+        database.executeQuery(query, (err, rows) => {
+            if(err){
+                const errorObject = {
+                    message: 'Er is iets misgegaan',
+                    code: 500
+                }
+                next(errorObject);
+            }            
+            if(rows){
+                res.status(200).json({result: rows});
+            }
+        });
+
+        console.log(query);
+
+    },
+
+    updateAppartment: (req, res, next) => {
+
+        console.log("update appartment called");
+
+        const appartment = req.body;
+        const id = req.params.id;
+
+        const query = `UPDATE Apartment SET Apartment.Description = '${appartment.Description}', Apartment.StreetAddress = '${appartment.StreetAddress}', Apartment.PostalCode = '${appartment.PostalCode}', Apartment.City = '${appartment.City}' WHERE Apartment.ApartmentId = ${id};`;
+
+        database.executeQuery(query, (err, rows) => {
+
+            if(err){
+                const errorObject = {
+                    message: 'Er is iets misgegaan',
+                    code: 500
+                }
+                next(errorObject);
+            }else{
+                res.status(200).json({result: appartment});
+            }
+        })
+    },
+
+    //TODO: Authenticate user, he's the only one to delete it
+    deleteAppartment: (req, res, next) => {
+        
+        console.log("deleteAppartment");
+
+        const id = req.params.id;
+        const query = `DELETE FROM Apartment WHERE Apartment.ApartmentId=${id}`;
+
+        database.executeQuery(query, (err, rows) => {
+
+            if(err){
+                const errorObject = {
+                    message: 'Er is iets misgegaan',
+                    code: 500
+                }
+                next(errorObject);
+            }else{
+                res.status(200).json({});
+            }
+        })
+    },
+
+    createReservation: (req, res, next) => {
+
+        const id = req.params.id;
+        const reservation = req.body;
+
+        const query = `INSERT INTO Reservation VALUES(${id}, '${reservation.StartDate}', '${reservation.EndDate}', '${reservation.Status}', ${reservation.UserId});`;
+
+        console.log(query);
+
+        database.executeQuery(query, (err, rows) => {
+
+            if(err){
+                const errorObject = {
+                    message: 'Er is iets misgegaan',
+                    code: 500
+                }
+                next(errorObject);
+            }else{
+                res.status(200).json({});
+            }
+        })
+    },
+
+    getAllReservationsByAppartment: (req, res, next) => {
+
+        console.log("getAllReservationsByAppartment");
+
+        const id = req.params.id;
+        query = `SELECT * FROM Reservation WHERE Reservation.ApartmentId = ${id};`;
+
+        database.executeQuery(query, (err, rows) => {
+
+            if(err){
+                const errorObject = {
+                    message: 'Er is iets misgegaan',
+                    code: 500
+                }
+                next(errorObject);
+            }else{
+                res.status(200).json({Result: rows});
+            }
+        })
+    },
+
+    getReservationByAppartment: (req, res, next) => {
+
+        console.log("getReservationByAppartment");
+
+        const id = req.params.id;
+        const rid = req.params.rid;
+
+        const query = `SELECT * FROM Reservation WHERE Reservation.ApartmentId = ${id} AND Reservation.ReservationId = ${rid};`;
+
+        database.executeQuery(query, (err, rows) => {
+
+            if(err){
+                const errorObject = {
+                    message: 'Er is iets misgegaan',
+                    code: 500
+                }
+                next(errorObject);
+            }else{
+                res.status(200).json({Result: rows});
+            }
+        })        
+    },
+
+    updateReservationStatus: (req, res, next) => 
+    {
+        console.log("updateReservationStatus");
+
+        const id = req.params.id;
+        const rid = req.params.rid;
+        const Registration = req.body;
+
+        const query = `UPDATE Reservation SET Reservation.Status = '${Registration.Status}' WHERE Reservation.reservationId=${rid} AND Reservation.ApartmentId=${id};`;
+
+        database.executeQuery(query, (err, rows) => {
+
+            if(err){
+                const errorObject = {
+                    message: 'Er is iets misgegaan',
+                    code: 500
+                }
+                next(errorObject);
+            }else{
+                res.status(200).json({});
+            }
+        })
+    },
+
+    deleteReservationByAppartment:  (req, res, next) => {
+
+        console.log("updateReservationStatus");
+
+        const id = req.params.id;
+        const rid = req.params.rid;
+        
+        const query = `DELETE FROM Reservation WHERE Reservation.reservationId = ${rid} AND Reservation.apartmentId = ${id}`;
+
+        database.executeQuery(query, (err, rows) => {
+
+            if(err){
+                const errorObject = {
+                    message: 'Er is iets misgegaan',
+                    code: 500
+                }
+                next(errorObject);
+            }else{
+                res.status(200).json({});
+            }
+        })
+
+    }
+}
