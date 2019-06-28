998261e4b046185a5a25a4ce6daa05334d04adef
totatevenaar <jordyvanekelen1@gmail.com>
2019-06-28 17:34:44 +0200

    PORT

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const authRoutes = require('./src/routes/auth.routes')
 const appartmentRoutes = require('./src/routes/appartments.routes');
 
 const app = express();
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
